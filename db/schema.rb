a63a9eea7933d1357c35e70b7768c56405ec2b82
conor.hickey1@gmail.com <conor.hickey1@gmail.com>
2015-10-21 13:49:35 +0100

    added quantities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021121631) do
+ActiveRecord::Schema.define(version: 20151021122708) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
